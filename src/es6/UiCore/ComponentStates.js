4b8c6404ca62b0a904f161d463366b8c2de9c743
dasiux <me@siux.info>
2022-04-07 22:26:54 +0200

    ui states + class states

---
+++ src/es6/UiCore/ComponentStates.js
@@ -0,0 +1,217 @@
+/**
+ * Requires
+ */
+import { isPojo } from '../Object/isPojo.js';
+import { Exception } from '../Error/Exception.js';
+import { mergeObject } from '../Object/mergeObject.js';
+
+/**
+ * Component states exception
+ * @class
+ */
+class ComponentStatesException extends Exception {}
+
+/**
+ * @typedef {Object|true} ComponentStateDefinition - Component state definition
+ * @property {undefined|null|boolean} global - By default state is global, set to: false for non global states
+ * @property {undefined|null|string} classOn - CSS class to set when active
+ * @property {undefined|null|string} classOff - CSS class to set when inactive
+ * @property {undefined|null|Array<string>} unsets - Unset given states when this one becomes active
+ */
+
+/**
+ * Component states
+ * @class
+ */
+export class ComponentStates {
+
+    /**
+     * Component element
+     * @private
+     * @property
+     * @type {null}
+     */
+    #component = null;
+
+    /**
+     * Component states
+     * @private
+     * @property
+     * @type {null}
+     */
+    #states = null;
+
+    /**
+     * States by name
+     * @private
+     * @property
+     * @type {Object}
+     */
+    #named = {};
+
+    /**
+     * Global state
+     * @private
+     * @property
+     * @type {null|string}
+     */
+    #global = null;
+
+    /**
+     * Global state attribute
+     * @private
+     * @property
+     * @type {string}
+     */
+    #attribute = 'data-state';
+
+    /**
+     * Constructor
+     * @constructor
+     * @param {UiComponent} component - Component element
+     * @param {null|Object} states - States map
+     */
+    constructor( component, states = null ) {
+        this.#component = component;
+        if ( states !== null && !isPojo( states ) ) {
+            throw new ComponentStatesException( 'Argument states must be null or a plain Object' );
+        } else if ( states === null ) {
+            states = { initialized : { classOn : 'initialized' } };
+        }
+        this.#states = states;
+    }
+
+    /**
+     * Global state getter
+     * @public
+     * @return {null|string} - Global state
+     */
+    get global() {
+        return this.#global;
+    }
+
+    /**
+     * Direct states access
+     * @public
+     * @return {Object} - States data object
+     */
+    get exposed() {
+        return this.#states;
+    }
+
+    /**
+     * Extend states
+     * @public
+     * @param {Object} states - States map
+     * @return {void}
+     */
+    extend( states ) {
+        mergeObject( this.#states, states, true, true, true, false );
+    }
+
+    /**
+     * State active
+     * @public
+     * @param {string} name - State name
+     * @return {boolean} - State is active
+     */
+    is( name ) {
+        return !!this.#named[ name ];
+    }
+
+    /**
+     * State defined
+     * @public
+     * @param {string} name - State name
+     * @return {boolean} - State exists
+     */
+    has( name ) {
+        return !!this.#states[ name ];
+    }
+
+    /**
+     * Get state info
+     * @public
+     * @param {string} name - State name
+     * @return {Object} - State info object
+     */
+    get( name ) {
+        const state = this.#states[ name ];
+        if ( !state ) throw new ComponentStatesException( 'Unknown state: ' + name );
+        return state;
+    }
+
+    /**
+     * Set state by name
+     * @public
+     * @param {string} name - State name
+     * @return {void}
+     */
+    set( name ) {
+        const state = this.get( name );
+        const is_global = state === true || state.global !== false;
+        let is_global_changed = false, to = null;
+        const from = this.#global;
+        if ( is_global ) {
+            if ( name !== from ) {
+                is_global_changed = true;
+                to = name;
+            } else {
+                return;
+            }
+        }
+
+        // Complex state options
+        if ( state !== true ) {
+            if ( state.unsets instanceof Array ) {
+                for ( let i = 0; i < state.unsets.length; i++ ) {
+                    this.unset( state.unsets[ i ] );
+                }
+            }
+            if ( state.classOn ) this.#component.dom.classList.add( state.classOn );
+            if ( state.classOff ) this.#component.dom.classList.remove( state.classOff );
+        }
+
+        // Default and global states are both global
+        if ( is_global ) {
+            this.#component.dom.setAttribute( this.#attribute, name );
+            this.#global = name;
+        }
+
+        // Set individual state
+        this.#named[ name ] = true;
+        if ( is_global_changed ) {
+            this.#component.dispatchEvent( 'state.changed', { from, to } );
+        } else {
+            this.#component.dispatchEvent( 'state.set', { set : name } );
+        }
+    }
+
+    /**
+     * Set state by name
+     * @public
+     * @param {string} name - State name
+     * @return {void}
+     */
+    unset( name ) {
+        if ( !this.#named[ name ] ) return;
+        const state = this.get( name );
+        const is_global = state === true || state.global !== false;
+
+        // Complex state options
+        if ( state !== true ) {
+            if ( state.classOn ) this.#component.dom.classList.remove( state.classOn );
+            if ( state.classOff ) this.#component.dom.classList.add( state.classOff );
+        }
+
+        // Default and global states are both global
+        if ( is_global ) {
+            this.#component.dom.setAttribute( this.#attribute, 'null' );
+            this.#global = null;
+        }
+
+        // Set individual state
+        this.#named[ name ] = false;
+        this.#component.dispatchEvent( 'state.unset', { unset : name } );
+    }
+}
---
+++ src/es6/UiCore/ElementClassStates.js
@@ -0,0 +1,190 @@
+/**
+ * Requires
+ */
+import { isPojo } from '../Object/isPojo.js';
+import { Exception } from '../Error/Exception.js';
+import { mergeObject } from '../Object/mergeObject.js';
+import { requireUniqid } from '../HTML/uniqid.js';
+
+/**
+ * Element class states exception
+ * @class
+ */
+class ElementClassStatesException extends Exception {}
+
+/**
+ * @typedef {Object|true} ElementClassStateDefinition - Component state definition
+ * @property {undefined|null|string} classOn - CSS class to set when active
+ * @property {undefined|null|Array<string>} unsets - Unset given states when this one becomes active
+ */
+
+/**
+ * Element class states
+ * @class
+ */
+export class ElementClassStates {
+
+    /**
+     * Class states
+     * @private
+     * @property
+     * @type {null|Object}
+     */
+    #states = null;
+
+    /**
+     * Auto unset reference data
+     * @private
+     * @property
+     * @type {Object}
+     */
+    #autoUnsetRefs = {};
+
+    /**
+     * Default timeout delay
+     * @private
+     * @property
+     * @type {number}
+     */
+    #defaultTimeout = 600;
+
+    /**
+     * Constructor
+     * @constructor
+     * @param {null|Object} states - States map
+     */
+    constructor( states = null ) {
+        if ( !isPojo( states ) ) {
+            throw new ElementClassStatesException( 'Argument states must be null or a plain Object' );
+        }
+        this.#states = states;
+    }
+
+    /**
+     * Direct states access
+     * @public
+     * @return {Object} - States data object
+     */
+    get exposed() {
+        return this.#states;
+    }
+
+    /**
+     * Extend states
+     * @public
+     * @param {Object} states - States map
+     * @return {void}
+     */
+    extend( states ) {
+        mergeObject( this.#states, states, true, true, true, false );
+    }
+
+    /**
+     * State active
+     * @public
+     * @param {string} name - State name
+     * @param {HTMLElement} element - Element target
+     * @return {boolean} - State is active
+     */
+    is( name, element ) {
+        const state = this.get( name );
+        if ( state.classOn ) {
+            return element.classList.contains( state.classOn );
+        }
+        return false;
+    }
+
+    /**
+     * State defined
+     * @public
+     * @param {string} name - State name
+     * @return {boolean} - State exists
+     */
+    has( name ) {
+        return !!this.#states[ name ];
+    }
+
+    /**
+     * Get state info
+     * @public
+     * @param {string} name - State name
+     * @return {Object} - State info object
+     */
+    get( name ) {
+        const state = this.#states[ name ];
+        if ( !state ) throw new ElementClassStatesException( 'Unknown state: ' + name );
+        return state;
+    }
+
+    /**
+     * Set state by name
+     * @public
+     * @param {string} name - State name
+     * @param {HTMLElement} element - Element target
+     * @return {void}
+     */
+    set( name, element ) {
+        const state = this.get( name );
+
+        // Unset states
+        if ( state.unsets instanceof Array ) {
+            for ( let i = 0; i < state.unsets.length; i++ ) {
+                this.unset( state.unsets[ i ], element );
+            }
+        }
+
+        // Set state class
+        if ( state.classOn ) element.classList.add( state.classOn );
+
+        // Callback on
+        if ( state.callbackOn ) state.callbackOn( name, element, state );
+
+        // Automatic unset
+        if ( state.autoUnset ) {
+
+            // Get or create unique element id
+            const id = requireUniqid( element, 'ecs-', true );
+
+            // Clear any existing timeout
+            this.#clear_auto_unset( id );
+
+            // Setup new timeout for remove
+            const timeout = typeof state.autoUnset === 'number' ? state.autoUnset : this.#defaultTimeout;
+            this.#autoUnsetRefs[ id ] = window.setTimeout( () => {
+
+                // Clear and unset state if reached
+                this.#clear_auto_unset( id );
+                this.unset( name, element );
+            }, timeout );
+        }
+    }
+
+    /**
+     * Clear auto unset timeout
+     * @param {string} id - Element id
+     * @return {void}
+     */
+    #clear_auto_unset( id ) {
+        if ( this.#autoUnsetRefs[ id ] ) {
+            window.clearTimeout( this.#autoUnsetRefs[ id ] );
+            this.#autoUnsetRefs[ id ] = null;
+        }
+    }
+
+    /**
+     * Set state by name
+     * @public
+     * @param {string} name - State name
+     * @param {HTMLElement} element - Element target
+     * @return {void}
+     */
+    unset( name, element ) {
+        const state = this.get( name );
+
+        // Complex state options
+        if ( state.classOn ) element.classList.remove( state.classOn );
+
+        // Callback off
+        if ( state.callbackOff ) state.callbackOff( name, element, state );
+    }
+}
